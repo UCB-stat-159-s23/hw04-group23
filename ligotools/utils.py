d534b7e3a96ce5897850a94c111ca5948c6f4499
Jiachen Li <jiac.li@berkeley.edu>
2023-04-14 05:34:35 +0000

    Fix merge conflicts and update notebook

--- ligotools/utils.py
+++ ligotools/utils.py
@@ -57,7 +57,7 @@ def plot_results(timemax, SNR, pcolor, det, tevent, strain_whitenbp, template_ma
     plt.grid('on')
     plt.xlabel('Time since {0:.4f}'.format(timemax))
     plt.legend(loc='upper left')
-    plt.savefig(eventname+"_"+det+"_SNR."+plottype)
+    plt.savefig('figures/' + eventname+"_"+det+"_SNR."+plottype)
 
     plt.figure(figsize=(10,8))
     plt.subplot(2,1,1)
@@ -80,7 +80,7 @@ def plot_results(timemax, SNR, pcolor, det, tevent, strain_whitenbp, template_ma
     plt.ylabel('whitened strain (units of noise stdev)')
     plt.legend(loc='upper left')
     plt.title(det+' Residual whitened data after subtracting template around event')
-    plt.savefig(eventname+"_"+det+"_matchtime."+plottype)
+    plt.savefig('figures/' +eventname+"_"+det+"_matchtime."+plottype)
 
     # -- Display PSD and template
     # must multiply by sqrt(f) to plot template fft on top of ASD:
@@ -95,4 +95,4 @@ def plot_results(timemax, SNR, pcolor, det, tevent, strain_whitenbp, template_ma
     plt.ylabel('strain noise ASD (strain/rtHz), template h(f)*rt(f)')
     plt.legend(loc='upper left')
     plt.title(det+' ASD and template around event')
-    plt.savefig(eventname+"_"+det+"_matchfreq."+plottype)
+    plt.savefig('figures/' +eventname+"_"+det+"_matchfreq."+plottype)
